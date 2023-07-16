15ed1aa2244bc0f2d59b52713842c4842c8241e1
pojono <pojono@gmail.com>
2023-07-16 16:42:41 +0200

    add aws

--- src/lib/bot.names.ts
+++ src/lib/bot.names.ts
@@ -1,5 +1,9 @@
 import { tryGet } from './try.get';
 
 export function botNames(): string[] {
+  const env = process.env['BOT_NAME'];
+  if (env) {
+    return [env];
+  }
   return tryGet<string[]>('bot.names') || [];
 }
