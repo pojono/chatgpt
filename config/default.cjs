2ff57784183d463050d4b8124716250d80d433e6
pojono <pojono@gmail.com>
2023-07-19 23:55:55 +0200

    delete system message config

--- config/default.cjs
+++ config/default.cjs
@@ -14,7 +14,6 @@ module.export = {
       "apiKey": process.env['OPENAI_API_KEY'],
       "apiBaseUrl": "",
       "completionParams": {},
-      "systemMessage": "",
       "maxModelTokens": 0, // set to 0 to use default
       "maxResponseTokens": 0, // set to 0 to use default
       "timeoutMs": 60000 // set to 0 to disable
