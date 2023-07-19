166dbc83d57ee2d691de1f6c2f8fd3a26d310242
pojono <pojono@gmail.com>
2023-07-19 23:52:28 +0200

    add get default prompt

---
+++ src/lib/get.default.prompt.ts
@@ -0,0 +1,7 @@
+import { FileData } from './read.files';
+
+export function getDefaultPrompt(prompts: FileData): string {
+  return 'default' in prompts
+    ? prompts.default
+    : 'You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.';
+}
