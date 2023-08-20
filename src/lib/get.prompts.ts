649941caf5b14ac7ca6b6c5ebc7abbd3bafd328a
ivan <ivan@eyeq.photos>
2023-08-20 17:53:16 +0200

    get prompts

--- src/lib/get.prompts.ts
+++ src/lib/get.prompts.ts
@@ -1,9 +1,13 @@
-import { FileData, readFiles } from './read.files.js';
-import { fileURLToPath } from 'url';
+import { FileData } from './read.files.js';
 import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { fetchGoogleSheet } from './get.sheet.js';
 
-export function getPrompts(): FileData {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  return readFiles(path.join(__dirname, '../../prompt'));
+export async function getPrompts(): Promise<FileData> {
+  const sheetUrl =
+    'https://docs.google.com/spreadsheets/d/1eroM0RfTK0YMHG6hHRoB7ubkBt_5pG9GTHTrTY7sikM/edit';
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const privateKeyPath = path.join(__dirname, '../../prompt', 'key.json');
+  return fetchGoogleSheet(sheetUrl, privateKeyPath);
 }
