a1f56c69e1a452f5a07981fb12bfa3308a60bf0e
ivan <ivan@eyeq.photos>
2024-09-13 16:32:16 +0300

    try o1

--- src/utils.ts
+++ src/utils.ts
@@ -9,7 +9,7 @@ function loadConfig(): Config {
       process.env.OPENAI_API_KEY ?? config.get<string>('api.official.apiKey'),
     apiBaseUrl: tryGet<string>('api.official.apiBaseUrl'),
     completionParams: {
-      model: 'gpt-4o',
+      model: 'o1-preview',
     },
     maxModelTokens: 16000,
     maxResponseTokens: 1000,
